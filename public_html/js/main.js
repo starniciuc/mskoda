65c244c60fd76a00d953c24304b5078ffa59a5c1
mstarniciuc <mstarniciuc@mstarniciuc-System-Product-Name>
2014-07-28 15:26:16 +0300

    slider

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function() {
 
+	$('.skodaSlider').iosSlider({
+		snapToChildren: true,
+		desktopClickDrag: true,
+		keyboardControls: true,
+		navNextSelector: $('.slideRight'),
+		navPrevSelector: $('.slideLeft'),
+		navSlideSelector: $('.selectors .item'),
+		onSlideChange: slideChange,
+		infiniteSlider: true,
+		responsiveSlides: true
+	});
+
+});
+
+function slideChange(args) {
+
+	$('.selectors .item').removeClass('selected');
+	$('.selectors .item:eq(' + (args.currentSlideNumber - 1) + ')').addClass('selected');
+
+}
